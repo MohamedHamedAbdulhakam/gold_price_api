48baf3ab0eabeef3e14614dbe285c60ba260d8e8
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-11-15 10:29:28 +0200

    fininsh ui

--- lib/home_screen_body.dart
+++ lib/home_screen_body.dart
@@ -5,23 +5,120 @@ class HomeScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  backgroundColor: Colors.black,
-      appBar: AppBar(title: Center(child: Text('TODAY PRICE')),),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title:  Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              
+              children: [
+                 Text('PRICE',  style: TextStyle(
+                      color: Colors.white,
+                      fontSize:24,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.yellow[300]!,
+                          offset: Offset(1, 1),
+                        )
+                      ]),),
+                      SizedBox(width: 5,),
+
+                         Text(
+                  'TODAY',
+                  style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize:24,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.yellow[300]!,
+                          offset: Offset(1, 1),
+                        )
+                      ]),
+                ),
+              ],
+            )),
+      ),
       body: Container(
-        margin: EdgeInsets.all(10),
+        padding: const EdgeInsets.fromLTRB(50,20,20,50),
+        margin: const EdgeInsets.all(10),
         child: Row(
-          children: [Column(
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                child: Image.asset('assets/gold.png')),
-            ],
-          )],
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Image.asset(
+                  'assets/gold.png',
+                  height: MediaQuery.of(context).size.height / 10,
+                  width: MediaQuery.of(context).size.width / 4,
+                ),
+                SizedBox(height: 20,),
+                Text(
+                  'Gold',
+                  style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize:MediaQuery.of(context).size.width/10,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.yellow[300]!,
+                          offset: Offset(1, 1),
+                        )
+                      ]),
+                ),
+                Text('Price \$',  style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontSize:MediaQuery.of(context).size.width/20,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.yellow[300]!,
+                          offset: Offset(1, 1),
+                        )
+                      ]),)
+              ],
+            ),
+            SizedBox(width: 100,),
+            Column(
+              children: [
+                Image.asset(
+                  'assets/silver.png',
+                  height: MediaQuery.of(context).size.height / 10,
+                  width: MediaQuery.of(context).size.width / 4,
+                ),
+                SizedBox(height: 20,),
+                Text(
+                  'Silver',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize:MediaQuery.of(context).size.width/10,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.grey[300]!,
+                          offset: Offset(1, 1),
+                        )
+                      ]),
+                ),
+                Text('Price \$',  style: TextStyle(
+                      color: Colors.white,
+                      fontSize:MediaQuery.of(context).size.width/20,
+                      fontWeight: FontWeight.bold,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.yellow[300]!,
+                          offset: Offset(1, 1),
+                        )
+                      ]),)
+              ],
+            )
+          ],
         ),
       ),
-    
     );
-    
   }
 }
